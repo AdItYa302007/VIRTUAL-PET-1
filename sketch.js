9d2a0e0b6045cc13d15e0b319b34d744a8ca73c6
AdItYa302007 <66154816+AdItYa302007@users.noreply.github.com>
2020-07-23 20:51:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var dog;
+var happyDog;
+var database;
+var foodS,foodStock;
+
+function preload()
+{
+dog_img1 = loadImage("images/dogImg.png");
+dog_img2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  dog = createSprite(250,250,50,50);
+  dog.addImage(dog_img1);
+  var foodStockRef = database.ref("Food")
+  foodStockRef.on("value",(data)=>{
+  foodS = data.val();
+  });
+
+}
+
+
+function draw() {  
+background(46,139,87);
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+dog.addImage(dog_img2);
+}
+drawSprites();
+fill("red");
+text("FOOD REMAINING"+foodS,200,210);
+
+}
+
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x= x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
